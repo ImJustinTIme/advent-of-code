05c09489f357ada7a358211e17033308bd7b19a3
ImJustinTIme <jukuennen@gmail.com>
2023-12-09 14:27:07 -0600

    day 3 go init

--- 2023/day-02/src/bin/part2.rs
+++ 2023/day-02/src/bin/part2.rs
@@ -9,27 +9,36 @@ fn get_int_from_string(string: &str) -> i32 {
     digits.parse().unwrap()
 }
 
-fn check_if_possible(game_line: &str) -> i32 {
+fn get_min_blocks(game_line: &str) -> i32 {
     let mut game_number_list = game_line.split(':');
-    let game_num = get_int_from_string(game_number_list.next().unwrap());
+    game_number_list.next();
     let games_list = game_number_list.next().unwrap().split(';');
 
+    let mut min_red = 1;
+    let mut min_green = 1;
+    let mut min_blue = 1;
     for game in games_list.into_iter() {
         let entries = game.split(',');
         for entry in entries.into_iter() {
             let number_of_color = get_int_from_string(entry);
 
-            let over_red = entry.contains("red") && number_of_color > 12;
-            let over_green = entry.contains("green") && number_of_color > 13;
-            let over_blue = entry.contains("blue") && number_of_color > 14;
+            let over_red = entry.contains("red") && number_of_color > min_red;
+            let over_green = entry.contains("green") && number_of_color > min_green;
+            let over_blue = entry.contains("blue") && number_of_color > min_blue;
 
-            if (over_red || over_blue || over_green) {
-                return 0;
+            if over_red {
+                min_red = number_of_color;
+            }
+            if over_blue {
+                min_blue = number_of_color;
+            }
+            if over_green {
+                min_green = number_of_color;
             }
         }
     }
 
-    game_num
+    min_red * min_blue * min_green
 }
 
 fn main() -> std::io::Result<()> {
@@ -37,7 +46,7 @@ fn main() -> std::io::Result<()> {
 
     let input_list = BufReader::new(file).lines();
     let results: i32 = input_list
-        .map(|text_line| check_if_possible(&text_line.unwrap()))
+        .map(|text_line| get_min_blocks(&text_line.unwrap()))
         .sum();
 
     println!("resuts: {}", results);
